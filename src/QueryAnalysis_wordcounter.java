397a5280680a1eefd746e1599ce81a737ff4869e
hsu <edwin1998516@gmail.com>
2021-01-13 22:37:12 +0800

    first commit

---
+++ src/QueryAnalysis_wordcounter.java
@@ -0,0 +1,202 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+/*
+ * 1.
+ * 2.field: GoogleQ_v2 originalQuery;
+ *          String query;
+ *          ArrayList<Keyword> keywords;
+ * 3.constructor: 1.QueryAnalysis(String user_inputKW): 
+ *                  a.(1)初始化一個GoogleQ_v2(使用者輸入的keyword)，並用GoogleQ_v2的query()將call google後
+ *                     抓下來的url+title放入originalQuery的ArrayList<UrlInf> urlList
+ *                    (2)初始化keywords: 並放入所有的keyword包含使用者的輸入
+ *                    (3)呼叫getScore()將urlList裡面所有的url計算分數
+ *                  b.input:使用者輸入的keyword(權重最重:1)
+ *                  
+ *                2.QueryAnalysis(String user_inputKW,ArrayList<Keyword> k):先不管
+ * 4. method: void getScore()
+ *            GoogleQ_v2 getGoogleQuery()
+ */
+public class QueryAnalysis_wordcounter {
+	private GoogleQ_v2 originalQuery;
+	private String user_inputKW;
+	private ArrayList<Keyword> keywords;
+	
+	public QueryAnalysis_wordcounter(GoogleQ_v2 google) throws IOException{
+//		originalQuery=new GoogleQ_v2(user_inputKW.replace(" ", "+"),1);
+		originalQuery = google;
+		this.user_inputKW=originalQuery.returnKey();
+		originalQuery.query();
+		keywords=new ArrayList<Keyword>();
+		//因為有點卡先拿一個做測試就好
+		keywords.add(new Keyword("開箱",0,0.9));
+		keywords.add(new Keyword("新",0,0.5));
+		keywords.add(new Keyword("介紹",0,0.5));
+		keywords.add(new Keyword("新品",0,0.7));
+		keywords.add(new Keyword("寶",0,0.1));
+		keywords.add(new Keyword("比較",0,0.3));
+		keywords.add(new Keyword("測試",0,0.4));
+		keywords.add(new Keyword("看法",0,0.4));
+		keywords.add(new Keyword("感想",0,0.35));
+		keywords.add(new Keyword("評測",0,0.4));
+		keywords.add(new Keyword("揭",0,0.2));
+		keywords.add(new Keyword("夯",0,0.5));
+		keywords.add(new Keyword("實測",0,0.5));
+		keywords.add(new Keyword("人氣",0,0.35));
+		keywords.add(new Keyword("熱門",0,0.4));
+		keywords.add(new Keyword("分享",0,0.25));
+		keywords.add(new Keyword("unbox",0,0.9));
+		keywords.add(new Keyword("new",0,0.5));
+		keywords.add(new Keyword("introduction",0,0.5));
+		keywords.add(new Keyword("comparison",0,0.3));
+		keywords.add(new Keyword("test",0,0.4));
+		keywords.add(new Keyword("perspective",0,0.4));
+		keywords.add(new Keyword("thoughts",0,0.35));
+		keywords.add(new Keyword("comment",0,0.4));
+		keywords.add(new Keyword("uncover",0,0.2));
+		keywords.add(new Keyword("hot",0,0.5));
+		keywords.add(new Keyword("testing",0,0.5));
+		keywords.add(new Keyword("famous",0,0.35));
+		keywords.add(new Keyword("hit",0,0.4));
+		keywords.add(new Keyword("share",0,0.25));
+//		keywords.add(new Keyword(user_inputKW,0,1));//�ϥΪ̿�J��keyword.
+		getScore();  //先不算分數
+		urlSort(0, originalQuery.getUrls().size() - 1);
+	}
+//	public QueryAnalysis_wordcounter(GoogleQ_v2 google) throws IOException
+//	{
+////		originalQuery=new GoogleQ_v2(user_inputKW.replace(" ", "+"),1);
+//		originalQuery = google;
+//		this.user_inputKW=originalQuery.returnKey();
+//		originalQuery.query();
+//		keywords=new ArrayList<Keyword>();
+//		//因為有點卡先拿一個做測試就好
+//		keywords.add(new Keyword("開箱",0,0.9));
+//		keywords.add(new Keyword("新",0,0.5));
+//		keywords.add(new Keyword("介紹",0,0.5));
+//		keywords.add(new Keyword("新品",0,0.7));
+//		keywords.add(new Keyword("寶",0,0.1));
+//		keywords.add(new Keyword("比較",0,0.3));
+//		keywords.add(new Keyword("測試",0,0.4));
+//		keywords.add(new Keyword("看法",0,0.4));
+//		keywords.add(new Keyword("感想",0,0.35));
+//		keywords.add(new Keyword("評測",0,0.4));
+//		keywords.add(new Keyword("揭",0,0.2));
+//		keywords.add(new Keyword("夯",0,0.5));
+//		keywords.add(new Keyword("實測",0,0.5));
+//		keywords.add(new Keyword("人氣",0,0.35));
+//		keywords.add(new Keyword("熱門",0,0.4));
+//		keywords.add(new Keyword("分享",0,0.25));
+//		keywords.add(new Keyword("unbox",0,0.9));
+//		keywords.add(new Keyword("new",0,0.5));
+////		keywords.add(new Keyword("introduction",0,0.5));
+////		keywords.add(new Keyword("comparison",0,0.3));
+////		keywords.add(new Keyword("test",0,0.4));
+////		keywords.add(new Keyword("perspective",0,0.4));
+////		keywords.add(new Keyword("thoughts",0,0.35));
+////		keywords.add(new Keyword("comment",0,0.4));
+////		keywords.add(new Keyword("uncover",0,0.2));
+////		keywords.add(new Keyword("hot",0,0.5));
+////		keywords.add(new Keyword("testing",0,0.5));
+////		keywords.add(new Keyword("famous",0,0.35));
+////		keywords.add(new Keyword("hit",0,0.4));
+////		keywords.add(new Keyword("share",0,0.25));
+////		keywords.add(new Keyword(user_inputKW,0,1));//�ϥΪ̿�J��keyword.
+//		getScore();  //先不算分數
+//		urlSort(0, originalQuery.getUrls().size() - 1);
+//	}
+	
+	public QueryAnalysis_wordcounter(String user_inputKW,ArrayList<Keyword> k) throws IOException
+	{
+		this.user_inputKW=user_inputKW;
+		originalQuery=new GoogleQ_v2(user_inputKW.replace(" ", "+"),1);
+		originalQuery.query();
+		keywords=k;
+		keywords.add(new Keyword(user_inputKW,0,1)); //�ϥΪ̿�J��keyword.
+		getScore();
+	}
+
+
+
+	public void getScore() throws IOException
+	{
+		for(UrlInf u:originalQuery.getUrls()) 
+		{	
+			System.out.println(u.getTitle());
+			System.out.println(u.getUrl());
+			//----------先不要by Hsu(測試完)------------------
+//			int a=u.getUrl().indexOf("http");
+//			int b=u.getUrl().indexOf("&sa"); 
+//			System.out.println("Url After sub: "+u.getUrl().substring(a,b));
+			//-----------------------------------
+//			System.out.println(u.getUrl().substring(a,b));
+			WordCounter w=new WordCounter("https://www.google.com.tw"+u.getUrl());
+//			TitleCounter t=new TitleCounter(u.getTitle()); 
+			double totalScore=0;
+			for(Keyword k:keywords)
+			{
+				try {
+					k.setCount(w.countKeyword(k.getName())); //countKeyword最耗時間
+					double count=k.getCount();
+					double weight = k.getWeight();
+					//------計算細節顯示by Hsu-----
+//					System.out.println("key_name:"+k.getName());
+//					System.out.println("key_count:"+count);
+//					System.out.println("key_weight:"+weight);
+//					System.out.println("----網頁內容------");
+//					System.out.println(w.fetchContent());
+					//--------------------------
+					totalScore+=count*weight;					
+				}
+				catch (Exception e)
+				{
+					continue;
+				}
+			}
+			//------設定urlList裡UrlInf的score
+			u.setScore(totalScore);
+			System.out.println(u.getScore()+"--after adding");
+			System.out.println("=================================");
+		}	
+	}
+	
+	public GoogleQ_v2 getGoogleQuery()
+	{
+		return originalQuery;
+	}
+	
+	public void urlSort(int leftbound, int rightbound) throws IOException {
+		if (originalQuery.getUrls().size() == 0) {
+			System.out.println("InvalidOperation");
+			return;
+		}
+		if (leftbound >= rightbound) {
+			return;
+		}
+		int i = leftbound;
+		int j = rightbound;
+		double key = originalQuery.getUrls().get(leftbound).getScore();
+		while (i != j) {
+			while (originalQuery.getUrls().get(j).getScore() < key && i < j) {
+				j -= 1;
+			}
+			while (originalQuery.getUrls().get(i).getScore() >= key && i < j) {
+				i += 1;
+			}
+			if (i < j) {
+				swap(i, j);
+			}
+		}
+		swap(leftbound, i);
+		urlSort(leftbound, i - 1);
+		urlSort(i + 1, rightbound);
+
+	}
+	
+	private void swap(int aIndex, int bIndex) throws IOException{
+		UrlInf temp = originalQuery.getUrls().get(aIndex);
+		originalQuery.getUrls().set(aIndex, originalQuery.getUrls().get(bIndex));
+		originalQuery.getUrls().set(bIndex, temp);
+	}
+	
+}
---
+++ src/WordCounter_4YT.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+/*
+ * 1.constructor給一個urlstring，用fetchContent()將url會搜到的內容return出來，
+ *   再用countKeyword(keyword)給一個keyword並回傳keyword在content出現的次數                    
+ * 2.field: urlStr、content_raw
+ * 3.constructor: 給urlStr
+ * 4.method: String fetchContent():將url的內容抓下來。
+ *           double countKeyword(String keyword): 將keyword出現的次數return
+ */
+public class WordCounter_4YT {
+	private String urlStr;
+    public String content_raw;
+    private String title;
+    //---test--
+    private TextProcess tProcess;
+    //---------
+    public WordCounter_4YT(String urlStr){
+    	this.urlStr = urlStr;
+    }
+    
+    /*
+     * goal: 獲得網頁內容並return
+     * output: 輸出urlStr抓下來的結果，用UTF-8編碼，存在retVal裡並輸出
+     */
+    public String fetchContent() throws IOException{
+		URL url = new URL(this.urlStr);
+		URLConnection conn = url.openConnection();
+		conn.setRequestProperty("User-agent", "Mozilla/5.0 Chrome/8.0 Safari/537.36");
+		conn.setRequestProperty("Accept-Charset", "UTF-8");
+		InputStream in = conn.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+		
+		String retVal = "";
+	
+		String line = null;
+		
+		while ((line = br.readLine()) != null){
+		    retVal = retVal + line; //試試看不要換行會不換比較好
+		}
+		//------test-------
+//		Document doc = Jsoup.parse(retVal); 
+//		System.out.println(doc.text());
+		//------test-------
+//		return retVal.replaceAll("[A-Za-z0-9]+\\\\p{Punct}\\\\s*|\\t|\\r|\\n","");
+		return retVal.replaceAll("\\p{Punct}","").replaceAll("\\s*|\t|\r|\n", "");
+//		return retVal;
+    }
+    /*
+     * goal: 獲得輸入keyword出現的次數，一次可以count一個keyword
+     * intput: input 要被count的keyword
+     * output: output keyword出現的次數
+     */
+    public double countKeyword(String keyword) throws IOException{
+		if (content_raw == null){
+		    content_raw = fetchContent();
+		}
+		//--test--
+//		System.out.println(content_raw);
+//		tProcess = new TextProcess(content_raw);
+//		tProcess.Process();
+		//--------
+		//To do a case-insensitive search, we turn the whole content and keyword into upper-case:
+		content_raw = content_raw.toUpperCase();
+		keyword = keyword.toUpperCase();
+		int retVal = 0; 
+		int pos;
+		while(content_raw.indexOf(keyword)!=-1) {
+			retVal++;
+			pos=content_raw.indexOf(keyword);
+			content_raw = content_raw.substring(pos+keyword.length(), content_raw.length());
+		}
+		return retVal;
+    }
+}
